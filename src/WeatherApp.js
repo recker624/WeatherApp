713c98a66d57d5f79eacd4284012b4aef90790eb
recker624 <52703428+recker624@users.noreply.github.com>
2023-07-02 18:02:08 +0530

    Some minor bugfixes and style changes.

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,4 +1,5 @@
 "use strict";
+// eslint-disable-next-line no-unused-vars
 import { mapToStyles } from "@popperjs/core/lib/modifiers/computeStyles";
 import { staticRenderer } from "./UIManager";
 import { eventManager } from "./EventManager";
